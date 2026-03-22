3156fe4b4f27a1c94d49e5c3e2bed18fa25a655d
design-dolphins <rika.design20@gmail.com>
2026-03-22 14:04:16 +0900

    Update 90-days-sw.js

--- 90-days-sw.js
+++ 90-days-sw.js
@@ -1,5 +1,5 @@
-const CACHE='90days-v1';
+const CACHE='90days-v2';
 const ASSETS=['./90-days-challenge.html','./90-days-manifest.json'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(cached=>cached||fetch(e.request)))});
+self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))});
